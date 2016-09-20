9fffd982a27f9566fb555529f025643917e47352
Manu Uzkudun <manuuzkudun@gmail.com>
2016-09-20 23:45:01 +0200

    Book list appears

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,13 +1,14 @@
-import React, { Container } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-
-export default class BookList extends Component{
+class BookList extends Component {
   
-  renderList(){
+  renderList() {
     return this.props.books.map( book => {
-      <li key={book.title} className="list-group-item">{book.title}</li>;
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      ); 
     });
-    
   }
   
   render() {
@@ -15,6 +16,17 @@ export default class BookList extends Component{
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    )
+    );
   }
 }
+
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BooksLists
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
